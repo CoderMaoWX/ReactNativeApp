c55c24a7b8ff7d14dd6461c170686e2babe117fc
maowangxin <maowx@okdeer.com>
2017-11-17 10:06:53 +0800

    修改ScrionList

--- studyDemojs/FlexDemo.js
+++ studyDemojs/FlexDemo.js
@@ -42,13 +42,18 @@ export default class FlexDemo extends Component {
       </Text>
   };
 
+
+  //默认情况下每行都需要提供一个不重复的key属性,不然会有警告,使用json中的title动态绑定key
+  _keyExtractor(item, index) {
+    return item.title+index
+  }
+
   render() {
-    //默认情况下每行都需要提供一个不重复的key属性,不然会有警告
     let sections = [
-      { key: "A", data: [{ title: "阿童木" , key: "阿童木"}, { title: "阿玛尼", key: "阿玛尼" }, { title: "爱多多", key: "爱多多" }] },
-      { key: "B", data: [{ title: "表哥", key: "表哥" }, { title: "贝贝", key: "贝贝d" }, { title: "表弟", key: "贝弟" }, { title: "表姐", key: "表姐" }, { title: "表叔", key: "表叔" }] },
-      { key: "C", data: [{ title: "成吉思汗", key: "贝贝" }, { title: "超市快递", key: "贝d贝" }] },
-      { key: "W", data: [{ title: "王磊", key: "王磊" }, { title: "王者荣耀", key: "王者荣耀" }, { title: "往事不能回味", key: "往事不能回味" },{ title: "王小磊", key: "王小磊" }, { title: "王中磊", key: "王中磊s" }, { title: "王大磊", key: "王中磊" }] },
+      { key: "A", data: [{ title: "阿童木"}, { title: "阿玛尼"}, { title: "爱多多"}] },
+      { key: "B", data: [{ title: "表哥"}, { title: "贝贝"}, { title: "表弟"}, { title: "表姐"}, { title: "表叔"}] },
+      { key: "C", data: [{ title: "成吉思汗" }, { title: "超市快递"}] },
+      { key: "W", data: [{ title: "王磊"}, { title: "王者荣耀"}, { title: "往事不能回味"},{ title: "王小磊"}, { title: "王中磊"}, { title: "王大磊"}] },
     ];
 
     return (
@@ -59,6 +64,7 @@ export default class FlexDemo extends Component {
               renderSectionHeader={this._renderSectionHeader}
               renderItem={this._renderItem}
               sections={sections}
+              keyExtractor={this._keyExtractor}  //使用json中的title动态绑定key
               ItemSeparatorComponent={() => <View style={{backgroundColor: '#e8e8e8', height: 1}} />}
               ListHeaderComponent={() => <View style={{ backgroundColor: '#25B960', justifyContent: 'center', height: 80 }}><Text style={{ fontSize: 18, alignSelf:'center', color: '#ffffff' }}>表格头部</Text></View>}
               ListFooterComponent={() => <View style={{ backgroundColor: '#25B960', justifyContent: 'center', height: 64 }}><Text style={{ fontSize: 18, alignSelf:'center', color: '#ffffff' }}>表格尾部</Text></View>}
